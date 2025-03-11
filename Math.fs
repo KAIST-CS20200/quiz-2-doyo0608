22d7fc898732662eef9ab2f3299766980a1b4a40
doyoung <doyoung0828@naver.com>
2025-03-11 09:51:28 +0900

    try 1

--- Math.fs
+++ Math.fs
@@ -6,4 +6,10 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let isEven (x : int64) = x % (int64 2) = 0
+  let num = int64 n
+  let rec iter (a : int64) count =
+    if a = 1 then count
+    else  if isEven a then iter (a/(int64 2)) (count+1)
+          else iter ((int64 3)*a+(int64 1)) (count+1)
+  iter num 0
